a459b06f71bc91d51d949479fa589d8e2c061b69
r0nz-29 <gs2019048@sgsitsindore.in>
2023-03-31 14:13:58 +0530

    init5

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-import express from "express";
+import express from 'express';
+import http from "http";
+import {Server} from "socket.io";
+import bodyParser from 'body-parser';
 import cors from "cors";
-import bodyParser from "body-parser";
+
+const port = process.env.PORT || 4001;
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-	res.send("<h1>Hello world</h1>");
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  }
 });
 
-app.post("/webhook", (req, res) => {
-	console.log("webhook aya");
-	console.log(req.body);
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
-app.get("/lol", (req, res) => {
-	res.json({msg: "lolest"});
+app.post('/webhook', (req, res) => {
+  const payload = req.body;
+  console.log(payload);
+  io.emit('new_meet', payload);
+  res.status(200).send('Success');
 });
 
-app.listen(3000, () => {
-	console.log("listening on *:3000");
-});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
